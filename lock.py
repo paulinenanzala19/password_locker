043bdd63bafa5da19a96340c3571fa3db1a48545
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-04-23 12:43:47 +0300

    amending indentation error

--- lock.py
+++ lock.py
@@ -1,20 +1,20 @@
 import random
 import string
 
-class user:
+class User:
     """
     Creating a class that generates new instances of a user
 
     """
     user_list=[]
 
-def __init__(self,username,password):
+    def __init__(self,username,password):
 
-    """
-    method that defines properties of a user
-    """
-    self.username=username
-    self.password=password
+      """
+      method that defines properties of a user
+      """
+      self.username=username
+      self.password=password
 
     
 
--- lock_test.py
+++ lock_test.py
@@ -1,20 +1,31 @@
 import unittest
-from lock import user
+from lock import User
 
-class TestClass(unittest,TestCase):
+class TestClass(unittest.TestCase):
     """
     Test class that defines test cases of the class user behaviours
 
     """
 
-def setUp(self):
-    """
-    method to run before individual test cases
+    def setUp(self):
 
-    """
-    self.new_user= user("paulinenanzala19","Syt45nfh")
-    """
-    create user object
-    """
+       """
+       method to run before individual test cases
+
+       """
+       self.new_user= User("paulinenanzala19","Syt45nfh")
+    # """
+    # create user object
+    # """
+
+    def test_init(self):
+       """
+       test case to test if the object has been initialized properly
+       """
+
+       self.assertEqual(self.new_user.username,"paulinenanzala19")
+       self.assertEqual(self.new_user.password, "Syt45nfh")
 
 
+if __name__ == '__main__':
+    unittest.main()
