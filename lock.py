9d3a8395fcec04ec8d30e81aa20d8409a5579b7b
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-04-23 10:40:55 +0300

    creating a method that defines properties of a user

--- lock.py
+++ lock.py
@@ -2,3 +2,19 @@ import random
 import string
 
 class user:
+    """
+    Creating a class that generates new instances of a user
+
+    """
+    user_list=[]
+
+def __init__(self,username,password):
+
+    """
+    method that defines properties of a user
+    """
+    self.username=username
+    self.password=password
+
+    
+
